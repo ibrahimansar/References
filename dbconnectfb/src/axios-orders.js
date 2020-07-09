6b5b577712b637d82f6f2e230387941175a11e35
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-07-09 10:44:19 +0530

    baseURL: updated

--- dbconnectfb/src/axios-orders.js
+++ dbconnectfb/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://vehicle-garage.firebaseio.com/'
+    baseURL: 'https://reference-55a2a.firebaseio.com/'
 })
 
 export default instance;
