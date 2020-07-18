200145b8285a65ec42538873fc4b51cd2d23977a
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-07-18 22:18:11 +0530

    .

---
+++ MERN/frontend/src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './MainHeader.css';
+
+const MainHeader = props => {
+    return (
+        <header className= "main-header"> Your Places</header>
+    )
+}
+
+export default MainHeader;
