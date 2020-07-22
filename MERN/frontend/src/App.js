1817d6933c38cc8d0f62d36ec0037de1c7868800
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-07-22 16:51:03 +0530

    .,.

--- MERN/frontend/src/App.js
+++ MERN/frontend/src/App.js
@@ -9,6 +9,7 @@ import {
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -16,18 +17,21 @@ const App = () => {
     <Router>
       <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/:userId/places"> 
-         <UserPlaces />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
