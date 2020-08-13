01747dcea76959bec055ed6697068addc886c0a7
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-08-13 20:40:39 +0530

    .

--- MERN/backend/app.js
+++ MERN/backend/app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +12,8 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,14 +35,17 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
-
-
 
 mongoose
   .connect(
@@ -53,4 +61,8 @@ mongoose
 
 
 
+
+
+
+  
   
